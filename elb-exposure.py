5c34faf1f8b38b9d7c1331d664d27320774dd6fa
h4m24 <eng.hfaouri@gmail.com>
2017-05-19 15:36:29 +0200

    every elb and its allowed ingress CIDR

--- elb-exposure.py
+++ elb-exposure.py
@@ -7,7 +7,11 @@ for ElbDocument in ElbClient.describe_load_balancers()['LoadBalancerDescriptions
 
     if ElbDocument['Scheme'] == "internet-facing":
         print(ElbDocument['LoadBalancerName'])
-        print(SgClient.describe_security_groups(GroupIds=ElbDocument['SecurityGroups'])['SecurityGroups'][0]['IpPermissions'][0]['IpRanges'])
+
+        # print(SgClient.describe_security_groups(GroupIds=ElbDocument['SecurityGroups'])['SecurityGroups'][0]['IpPermissions'][0]['IpRanges'])
+        for SgCidr in SgClient.describe_security_groups(GroupIds=ElbDocument['SecurityGroups'])['SecurityGroups'][0]['IpPermissions'][0]['IpRanges']:
+            print(SgCidr['CidrIp'])
+        print("\n")
         # break
 
 
